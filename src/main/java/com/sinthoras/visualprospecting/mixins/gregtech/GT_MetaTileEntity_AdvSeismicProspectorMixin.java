db135f96a9248f550be0973a634949359039449a
matt-159 <16054364+matt-159@users.noreply.github.com>
2026-07-25 20:28:45 -0600

    use server lookup

--- src/main/java/com/sinthoras/visualprospecting/mixins/gregtech/GT_MetaTileEntity_AdvSeismicProspectorMixin.java
+++ src/main/java/com/sinthoras/visualprospecting/mixins/gregtech/GT_MetaTileEntity_AdvSeismicProspectorMixin.java
@@ -77,7 +77,7 @@ public abstract class GT_MetaTileEntity_AdvSeismicProspectorMixin extends GT_Met
                         lastChunkCoordZ = chunkCoordZ;
                         provider.loadChunk(chunkCoordX,chunkCoordZ); // we load the chunk to make sure wqe have the data
                         int dimId = aBaseMetaTileEntity.getWorld().provider.dimensionId;
-                        final GT_OreVeinStats.Stats stats = ClientOreVeinStats.getVeinStats(aBaseMetaTileEntity.getWorld(), chunkCoordX, chunkCoordZ);
+                        final GT_OreVeinStats.Stats stats = GT_OreVeinStats.getOreVeinStatsInChunk(aBaseMetaTileEntity.getWorld(), chunkCoordX, chunkCoordZ);
 
                         VeinType veinType;
                         if (stats != null) {
